d0cc68d6de5a83075d1e438a8b86b32081fde409
GustavoW <guvieirawalter@gmail.com>
2025-03-19 20:34:46 -0300

    Implementei a lógica de lançamento de dados

--- JogoDosDados/Program.cs
+++ JogoDosDados/Program.cs
@@ -1,13 +1,69 @@
-﻿namespace JogoDosDados.Conso
+﻿namespace JogoDosDados.ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
-            
+            while (true)
+            {
+                //Cabeçalho
+                Console.WriteLine("================================");
+                Console.WriteLine("         Jogo dos dados         ");
+                Console.WriteLine("================================");
 
+                Console.Write("Pressione ENTER para jogar os dados...");
+                Console.ReadLine();
+                
+                int somadoUs = 0;
+                int somadoPc = 0;
 
+                //Gerar números
+                Random numeroAleatorio = new Random();
+
+                int numeroAleatorioPc = numeroAleatorio.Next(1, 7);
+                int numeroAleatorioUs = numeroAleatorio.Next(1, 7);
+
+                Console.WriteLine($"{numeroAleatorioPc}");
+
+                string mensagemFinal = "";
+
+                while (numeroAleatorioPc <= 25 && numeroAleatorioUs <= 25)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"o Número que a MÁQUINA tirou: {numeroAleatorioPc}");
+                    Console.WriteLine($"o Número que o USUÁRIO tirou: {numeroAleatorioUs}");
+
+                    somadoUs += numeroAleatorio.Next(1, 7);
+                    somadoPc += numeroAleatorio.Next(1, 7);
+
+                    Console.WriteLine($"A soma da MÁQUINA está em: {somadoPc}");
+                    Console.WriteLine($"A soma do USUÁRIO está em: {somadoUs}");
+
+                    Console.ReadLine();
+
+                    if (numeroAleatorioPc >= 25)
+                    {
+                        mensagemFinal = "Você perdeu mais sorte na próxima.";
+                        break;
+                    }
+                    else if (numeroAleatorioUs >= 25)
+                    {   
+                        mensagemFinal = "PARABÉNS VOCÊ GANHOU CARA";
+                        break;
+                    }
+                   
+                }
+                Console.WriteLine($"{mensagemFinal}");
+                Console.Write("Deseja tentar novamente ? (S/N): ");
+
+                string opcaoContinuar = Console.ReadLine().ToUpper().Trim();
+
+                if (opcaoContinuar != "S")
+                {
+                    break;
+                }
+
+            }
 
 
         }
